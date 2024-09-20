6b744912ac16fda21dbe1d7a3499238130032293
Randy Cullins <randycullins2018@gmail.com>
2024-09-20 17:26:12 -0600

    playing with matchers toBe and toEqual

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,13 @@ const sum = require("./sum");
 test("Adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("2 + 2 = 4", () => {
+  expect(2 + 2).toBe(4);
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
