194ae2d522ee22a3c610da12bac7d01dac0002bf
Randy Cullins <randycullins2018@gmail.com>
2024-09-21 07:15:30 -0600

    toBeFalsy and toBeTruthy matchers

--- sum.test.js
+++ sum.test.js
@@ -13,3 +13,13 @@ test("object assignment", () => {
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
+
+test("null is falsy", () => {
+  const n = null;
+  expect(n).toBeFalsy();
+});
+
+test("1 is truthy", () => {
+  const n = 1;
+  expect(n).toBeTruthy();
+});
